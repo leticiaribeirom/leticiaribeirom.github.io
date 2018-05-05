c33832f483babb253f4f66b988ba488bd84c64db
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 17:15:51 -0300

    email final version

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -1,6 +1,5 @@
 function enviaEmail() {
     'use strict';
-    var email_enviado = false;
     var name = $("input#name").val();
     var email = $("input#email").val();
     var message = $("textarea#message").val();
@@ -8,17 +7,14 @@ function enviaEmail() {
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Novo recado na página de contato",
-        "<h2>Olá, um visitante deixou um recado para você no site.<br><b>Nome:</b> " + name + "<br>" + "<b>Email:</b> " + email + "<br>" + "<b>Mensagem:</b> " + message + "<br></h2>", {
+        "<h3>Olá, um visitante deixou um recado para você no site.</h3><br><br><p><b>Nome:</b> " + name + "<br>" + "<b>Email:</b> " + email + "<br>" + "<b>Mensagem:</b> " + message + "<br></p>", {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done() {
                 alert("Email enviado com sucesso!");
-                $("input#name").value = "";
-                $("input#email").value = "";
-                $("textarea#message").value = "";
+                window.location.reload();
             }
         }
     );
-    window.setTimeout(function (){}, 1500);
     return false;
 
 }
