6936ed68bc1f64a50a2e1a005883ce5fb3519acd
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 12:26:06 -0300

    teste

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -2,11 +2,11 @@ function enviaEmail() {
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Mensagem recebida no site",
-        "this is the body",
+        "Nome:" + "leticia" + "/n" + "Email:",
         {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done(message) {
-                alert("sent");
+                alert("Email enviado com sucesso!");
             }
         }
     );
