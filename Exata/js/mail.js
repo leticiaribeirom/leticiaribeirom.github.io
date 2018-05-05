c5911298e3011a75c9a82b56f796f04e271c4b70
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 17:02:58 -0300

    erasing inputs values

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -12,6 +12,9 @@ function enviaEmail() {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done() {
                 alert("Email enviado com sucesso!");
+                $("input#name").value = "";
+                $("input#email").value = "";
+                $("textarea#message").value = "";
             }
         }
     );
