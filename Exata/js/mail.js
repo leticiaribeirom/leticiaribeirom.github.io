7c2be3f87cb31a6f02441daabc2e18a3aebf63ed
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 16:12:34 -0300

    adicionando valores escritos no form

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -1,9 +1,12 @@
 function enviaEmail() {
     var email_enviado = false;
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("input#message").val();
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Mensagem recebida no site",
-        "Nome: " + "leticia" + "<br>" + "Email: " + "" + "<br>" + "Mensagem: " + "" + "<br>",
+        "<b>Nome:</b> " + name + "<br>" + "Email: " + email + "<br>" + "Mensagem: " + message + "<br>",
         {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done(message) {
