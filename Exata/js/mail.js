60534e892608e55351686add83f2562d771151cf
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 15:54:35 -0300

    changing body to interpret html

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -2,7 +2,7 @@ function enviaEmail() {
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Mensagem recebida no site",
-        "Nome: " + "leticia" + "\r\n" + "Email: " + "" + "\r\n" + "Mensagem: " + "" + "\r\n",
+        "Nome: " + "leticia" + "<br>" + "Email: " + "" + "<br>" + "Mensagem: " + "" + "<br>",
         {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done(message) {
