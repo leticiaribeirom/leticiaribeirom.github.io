71760e3f3b7985a3b2f0bc8ec56661d2294fcc2f
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 16:41:47 -0300

    stop refresh

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -1,21 +1,21 @@
 function enviaEmail() {
     'use strict';
-    // var email_enviado = false;
+    var email_enviado = false;
     var name = $("input#name").val();
     var email = $("input#email").val();
     var message = $("textarea#message").val();
-    window.setTimeout(function () {
-        Email.send("contato@exataprojetos.com.br",
-            "letiiribeiro@gmail.com",
-            "Novo recado na página de contato",
-            "<h3>Olá, um visitante deixou um recado para você no site.</h3><br><br><h2><b>Nome:</b> " + name + "<br>" + "<b>Email:</b> " + email + "<br>" + "<b>Mensagem:</b> " + message + "<br></h2>",
-            {
-                token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
-                callback: function done() {
-                    alert("Email enviado com sucesso!");
-                }
+
+    Email.send("contato@exataprojetos.com.br",
+        "letiiribeiro@gmail.com",
+        "Novo recado na página de contato",
+        "<h3>Olá, um visitante deixou um recado para você no site.</h3><br><br><h2><b>Nome:</b> " + name + "<br>" + "<b>Email:</b> " + email + "<br>" + "<b>Mensagem:</b> " + message + "<br></h2>", {
+            token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
+            callback: function done() {
+                alert("Email enviado com sucesso!");
             }
-        );
-    }, 1000);
+        }
+    );
+    // window.setTimeout(function (){}, 1000);
+    return false;
 
 }
