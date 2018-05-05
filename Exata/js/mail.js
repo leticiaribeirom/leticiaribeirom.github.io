32ee5b1566c87eba1b03c8f8205f84feef9803f3
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 16:11:55 -0300

    teste de espera de envio de email

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -1,4 +1,5 @@
 function enviaEmail() {
+    var email_enviado = false;
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Mensagem recebida no site",
@@ -7,7 +8,9 @@ function enviaEmail() {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done(message) {
                 alert("Email enviado com sucesso!");
+                email_enviado = true;
             }
         }
     );
+    while(!email_enviado);
 }
