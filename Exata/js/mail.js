158f8b8d789fa0032f0d5a14357ef55579d94da1
Leticia Ribeiro <leticiaribeiro@aluno.unb.br>
2018-05-05 17:58:08 -0300

    email final version

--- Exata/js/mail.js
+++ Exata/js/mail.js
@@ -7,7 +7,7 @@ function enviaEmail() {
     Email.send("contato@exataprojetos.com.br",
         "letiiribeiro@gmail.com",
         "Novo recado na página de contato",
-        "<h3>Olá, um visitante deixou um recado para você no site.<br /><hr /><br /><b>Nome: </b> " + name + "<br>" + "<b>Email: </b> " + email + "<br>" + "<b>Mensagem: </b> " + message + "<br /><hr /></h3><br>", {
+        "<h3>Olá, um visitante deixou um recado para você no site.<br><br><hr /><br /><b>Nome: </b> " + name + "<br>" + "<b>Email: </b> " + email + "<br>" + "<b>Mensagem: </b> " + message + "<br><br><hr /></h3><br>", {
             token: "ef4c4cb8-fef6-4cc3-a85a-7d0c85c7ce5a",
             callback: function done() {
                 alert("Email enviado com sucesso!");
